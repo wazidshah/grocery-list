a1e173d18d04aaf73f701bb7237172a33c8b368b
wazid <wazidshh@gmail.com>
2020-03-28 16:08:52 +0530

    auto filter

--- js/main.js
+++ js/main.js
@@ -113,6 +113,101 @@
     "el": "#contact-list",
     data:{
         contactList: [...dataList]
+    },
+    mounted(){
+        document.getElementById("areas").addEventListener("change",function(e){
+            const type = document.querySelector("input[name='type']:checked").value;
+            let options = {
+                useExtendedSearch: true,
+                threshold: 0,
+                location: 0,
+                distance: 100,
+                minMatchCharLength: 1,
+                keys: [
+                    "type",
+                    "location"
+                ]
+            };
+    
+            let data = [...list.contactList];
+            let typeFiltered = "";
+            if(type != "all")
+            {
+                typeFiltered = data.filter(function(element){
+                    if(type==element.type)
+                    {
+                        return true;
+                    }
+            
+                    return false;
+                });
+            }
+            else
+            {
+                typeFiltered = [...dataList];
+            }
+    
+            var filteredData = new Fuse(typeFiltered,options);
+    
+            var result = filteredData.search(`'${areas.value}`)
+    
+            var finalResult = result.map(function(res){
+                return res.item;
+            });
+    
+            list.contactList = [...finalResult];
+      });
+
+        //for radio buttons
+        let radios = document.querySelectorAll("input[name='type']");
+        
+        radios.forEach(function(radio){
+            radio.addEventListener("click",function(e){
+                let type = e.target.value;
+                let options = {
+                    useExtendedSearch: true,
+                    threshold: 0,
+                    location: 0,
+                    distance: 100,
+                    minMatchCharLength: 1,
+                    keys: [
+                        "type",
+                        "location"
+                    ]
+                }
+                if(e.target.checked)
+                {
+                    let data = [...list.contactList];
+                    let typeFiltered = "";
+                    if(type != "all")
+                    {
+                        typeFiltered = data.filter(function(element){
+                            if(type==element.type)
+                            {
+                                return true;
+                            }
+                    
+                            return false;
+                        });
+                    }
+                    else
+                    {
+                        typeFiltered = [...dataList];
+                    }
+
+                    var filteredData = new Fuse(typeFiltered,options);
+    
+                    var result = filteredData.search(`'${areas.value}`)
+            
+                    var finalResult = result.map(function(res){
+                        return res.item;
+                    });
+            
+                    list.contactList = [...finalResult];
+                }
+            })
+        })
+
     }
   });
 
@@ -120,7 +215,6 @@
   const grocery = document.getElementById("grocery");
   const medical = document.getElementById("medical");
 
-  
   
 
   filterButton.addEventListener("click",function(e){
@@ -139,7 +233,7 @@
             "location"
         ]
     }
-    console.log(`"'${areas.value}"`);
+    // console.log(`"'${areas.value}"`);
 
     let typeFiltered = copyData.filter(function(element){
         if(type==element.type)
