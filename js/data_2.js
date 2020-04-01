79d3492ab07a6aa1257cfd8063e77ea4fb92e69e
wazid <wazidshh@gmail.com>
2020-04-01 11:19:27 +0530

    volunteer list updated

--- js/data_2.js
+++
@@ -1,1 +0,0 @@
-const dataList = [{"Timestamp":"27/03/2020 21:08:58","type":["medical","milk","grocery"],"whatsapp":"","shopName":"Ashwini Medico","anyImage":"","phone":["9960468459","2345678904","1234567899"],"location":"Near Canara Bank, Marcela, Goa"}];
